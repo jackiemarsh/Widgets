87b389812c23271d3c847bb542922a18b9af972b
jackiemarsh <jackie.m.marsh@gmail.com>
2021-10-21 13:37:58 -0700

    work on weather api

--- weather.jsx
+++ weather.jsx
@@ -1,8 +1,20 @@
 import React, { useState, useEffect } from 'react';
 
+const toQueryString = (obj) => {
+    const parts = [];
+    for (let i in obj) {
+        if (obj.hasOwnProperty(i)) {
+            parts.push(`${encodeURIComponent(i)}=${encodeURIComponent(obj[i])}`);
+        }
+    }
+    return parts.join('&');
+  }
+
 function Weather() {
 
+    const [weather, setWeather] = useState(null)
     // useEffect(navigator.geolocation.getCurrentPosition(), [])
+    // my api key: a41cd0fb11238b932ebc8c60d0c85b87
 }
 
 export default Weather;
